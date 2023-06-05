d20bb0feccb5d89bc9125601364d14d106644c50
jasperBailey <jasperbailey98@gmail.com>
2023-06-05 16:03:49 +0100

    first commit

---
+++ src/main/java/com/jaws/jobshark/JobsharkApplication.java
@@ -0,0 +1,13 @@
+package com.jaws.jobshark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsharkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsharkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaws/jobshark/JobsharkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaws.jobshark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsharkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
